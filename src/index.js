a63ff9017a4370a6212c5a167514a24efe4453fb
Sean Gillespie <sean@mistersg.net>
2016-12-10 21:29:15 -0500

    Update src/index
    Copy most of the code from redux examples

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import { render } from 'react-dom';
 
-ReactDOM.render(
+import App from './App';
+
+render(
   <App />,
   document.getElementById('root')
 );
