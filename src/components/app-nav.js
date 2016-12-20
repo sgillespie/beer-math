9e1e398d002f3755fd05fb645e6d1b1e715e9542
Sean Gillespie <sean@mistersg.net>
2016-12-20 14:22:00 -0500

    Update NavBar
     * Change markup to react-bootstrap
     * Add tests

--- src/components/app-nav.js
+++ src/components/app-nav.js
@@ -1,27 +1,23 @@
 import React from 'react';
+import Navbar, { Brand, Header } from 'react-bootstrap/lib/Navbar';
+import Nav from 'react-bootstrap/lib/Nav';
+import NavItem from 'react-bootstrap/lib/NavItem';
 
 export default function() {
   return (
-    <nav className="navbar navbar-default navbar-static-top">
-      <div className="container">
-        <div className="navbar-header">
-          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span className="sr-only">Toggle navigation</span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-          </button>
-          <a className="navbar-brand" href="#">🍺+🍺=🍻</a>
-        </div>
-        <div id="navbar" className="navbar-collapse collapse">
-          <ul className="nav navbar-nav">
-            <li className="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <Navbar>
+      <Header>
+        <Brand>
+          <a href="#">🍺+🍺=🍻</a>
+        </Brand>
+      </Header> 
+
+      <Nav>
+        <NavItem href="#" active>Home</NavItem>
+        <NavItem href="#about">About</NavItem>
+        <NavItem href="#contact">Contact</NavItem>
+      </Nav>
+    </Navbar>
   );
 }
 
