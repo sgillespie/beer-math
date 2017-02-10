99a22a7b6c39b813d06f59128291408ad6e5e170
Josh Reynolds <jshrynlds@gmail.com>
2017-02-09 22:41:57 -0500

    added fixedheader and drawer to layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Content, Header, Layout, Navigation } from 'react-mdl/lib/Layout';
+import { Content, Header, Layout, Navigation, Drawer } from 'react-mdl/lib/Layout';
 
 import GristCalculator from './GristCalculator';
 import RecipeTargets from './RecipeTargets';
 
 export default function() {
   return (
-    <Layout>
+    <Layout fixedHeader>
       <Header title="🍺+🍺=🍻">
         <Navigation>
           <a href="#">Home</a>
@@ -14,7 +14,15 @@ export default function() {
           <a href="#">Contact</a>
         </Navigation>
       </Header>
-
+	  
+      <Drawer title="🍺+🍺=🍻">
+        <Navigation>
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">Contact</a>
+        </Navigation>
+      </Drawer>
+	  
       <Content>
         <div className="page-content">
           <RecipeTargets />
