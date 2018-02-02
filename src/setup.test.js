0d625faf9397e3f9a4ddbf86ca19323d3095e4aa
Sean Gillespie <sean@mistersg.net>
2018-02-01 21:24:47 -0500

    Add some more to the test setup script

--- src/setup.test.js
+++ src/setup.test.js
@@ -1,4 +1,9 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-15';
+import chai from 'chai';
+import sinonChai from 'sinon-chai';
+
+chai.use(sinonChai);
+chai.should();
 
 configure({ adapter: new Adapter() });
